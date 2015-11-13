ed0309080eb2b6b007ba06f9c677a2984098db2d
Kevin Hernández Rostrán <kevinah95@gmail.com>
2015-11-13 11:53:36 -0600

    Incorporación Login

--- public/_core/app.js
+++ public/_core/app.js
@@ -1,28 +1,15 @@
-var app = angular.module('wimlApp', ['ngRoute', 'routeStyles', 'oc.lazyLoad']);
+var app = angular.module('wimlApp', ['ngRoute', 'oc.lazyLoad', 'jcs-autoValidate']);
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
     $routeProvider.when('/home', {
         templateUrl: '_core/components/home/homeView.html',
-        controller: 'homeCtrl',
-        resolve: {
-            loadModule: ['$ocLazyLoad', function($ocLazyLoad) {
-                return $ocLazyLoad.load({
-                    serie: true,
-                    files: [
-                        'assets/home/css/animate.min.css',
-                        'assets/home/css/creative.css',
-                        'assets/home/js/jquery.easing.min.js',
-                        'assets/home/js/jquery.fittext.js'
-                    ]
-                });
-            }]
-        }
+        controller: 'homeCtrl'
     });
-    /*$routeProvider.when('/login', {
+    $routeProvider.when('/login', {
         templateUrl: '_core/components/login/loginView.html',
-        css: '_core/components/login/loginStyles.css'
+        controller: 'loginCtrl'
     });
-    $routeProvider.when('/sign_up', {
+    /*$routeProvider.when('/sign_up', {
         templateUrl: '_core/components/sign_up/sign_upView.html',
         css: '_core/components/sign_up/sign_upStyles.css'
     });*/
@@ -30,10 +17,17 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
         redirectTo: '/home'
     });
 
-    
+
     // use the HTML5 History API
     $locationProvider.html5Mode(true);
 }]);
+
+app.run([
+    'bootstrap3ElementModifier',
+    function(bootstrap3ElementModifier) {
+        bootstrap3ElementModifier.enableValidationStateIcons(true);
+    }
+]);
 
 /*app.run(['$rootScope', function ($rootScope) {
 
---
+++ public/_core/components/login/loginCtrl.js
@@ -0,0 +1,21 @@
+app.controller('loginCtrl', function($scope, $ocLazyLoad) {
+
+    $ocLazyLoad.load({
+        serie: true, //If true load your files in serie otherwise parallel.
+        files: [
+            'assets/login/js/jquery.backstretch.min.js',
+            'assets/login/css/form-elements.css',
+            'assets/login/css/style.css',
+            'assets/login/js/scripts.js' //angular-form
+        ]
+    });
+
+    $scope.submit = function() {
+        if ($scope.signupFrm.$valid) {
+            console.log($scope.model);
+        }
+    }
+
+
+
+});
