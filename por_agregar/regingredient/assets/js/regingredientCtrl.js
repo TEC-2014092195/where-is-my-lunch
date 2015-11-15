2511ee59a6d517f99f3bcc087ea335103f01fbe5
Jose Aguilar <jm95aguilar@hotmail.com>
2015-11-15 14:38:44 -0600

    Registrar ingrediente, final

--- por_agregar/regingredient/assets/js/regingredientCtrl.js
+++ por_agregar/regingredient/assets/js/regingredientCtrl.js
@@ -1,17 +1,59 @@
 var app = angular.module("regingredient",[]);
 imgsrc = "";
+function ValidateFields() {
+          var x = document.getElementById("ingredientName").value;
+          if(x == null || x == '') {
+            return false;
+          } 
+          x = document.getElementById("stocks").value;
+          if(x == null || x == '') {
+            return false;
+          }
+          x = document.getElementById("price").value;
+          if(x == null || x == '') {
+            return false;
+          } 
+          if(imgsrc == "" || imgsrc == null){
+            return false;
+          }
+          return true;
+        };
+
+
 app.controller('regingredientCtrl', ['$scope','$http', '$location', function($scope, $http, $location) {
+
+  var ingredient = {};
+
   $scope.init = function() {
-      $http.get('/api/regingredient').success(function(data) { $scope.posts = data; console.log(data); });
       console.log("Ejecutado");
   };
+
   $scope.registerIngredient = function(){
-  	  console.log("Reg Ingredient");
+    if (ValidateFields()){
+      ingredient.ingredientName = $scope.ingredientName;
+      ingredient.photo = imgsrc;
+      ingredient.idRestaurant = 1; /*Id del restaurante seleccionado, guardado en el session storage*/
+      ingredient.stocks = $scope.stocks;
+      ingredient.price = $scope.price;
+      console.log(JSON.stringify(ingredient));
+      var $promise = $http.post('/api/regingredient', ingredient);
+      $promise.then(function(msg) {
+          console.log(msg.data);
+      });
+      alert("Producto Registrado");
+      $location.path('/home');
+    }
+    else{
+      alert("Complete todos los espacios");
+    }
+  	  
   };
   $scope.goBack = function(){
-  	  console.log("Go Back");
+  	  $location.path('/home');
   };
+
 }]);
+
 function previewFile() {
     var preview = document.querySelector('img');
     var file = document.querySelector('input[type=file]').files[0];
@@ -20,7 +62,6 @@ function previewFile() {
     reader.onloadend = function() {
         preview.src = reader.result;
         imgsrc = reader.result;
-        console.log(imgsrc);
     }
 
     if (file) {
