4ea40e5c40cff975da10cb61e56e151f3ba36f70
Kevin Hernández Rostrán <kevinah95@gmail.com>
2015-11-16 05:09:45 -0600

    Registro y login incorporados con base

--- public/_core/components/sign_up/sign_upCtrl.js
+++ public/_core/components/sign_up/sign_upCtrl.js
@@ -7,7 +7,16 @@ app.controller('sign_upCtrl', function($scope, $ocLazyLoad, $http) {
             'assets/sign_up/css/sign_upStyles.css'
         ]
     });
-    $scope.model = {};
+    $scope.model = {
+        email:"",
+        firstlastname: "",
+        myImage: "",
+        password: "",
+        secondlastname: "",
+        name: "",
+        phone: ""
+    };
+    $scope.myFile = "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTEwZTMwYjc1MCB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1MTBlMzBiNzUwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ1LjUiIHk9Ijc0LjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=";
     $scope.model.myImage = "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTEwZTMwYjc1MCB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1MTBlMzBiNzUwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ1LjUiIHk9Ijc0LjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=";
     var handleFileSelect = function(evt) {
         var file = evt.currentTarget.files[0];
@@ -22,25 +31,24 @@ app.controller('sign_upCtrl', function($scope, $ocLazyLoad, $http) {
     };
     angular.element(document.querySelector('#file')).on('change', handleFileSelect);
     $scope.submit = function() {
-        //if ($scope.signupFrm.$valid) {
-            /*$http.post('/api/users/register', $scope.model).then(function(response) {
-                console.log(response);
-            });*/
+        if ($scope.signupFrm.$valid) {
+            
             var fd = new FormData();
 
             //Take the first selected file
-            fd.append("image", $scope.model.myImage);
-            $scope.model.myImage = "";
-            fd.append("model", $scope.model);
+            //fd.append('file', $scope.model.myImage);
+            //fd.append('file',$scope.model.myImage);
+            fd.append('data', JSON.stringify($scope.model));
+            //$scope.model.myImage = "";
+            //fd.append("model", $scope.model);
+            
             $http({
                 method: 'POST',
                 url: '/api/users/register',
-                data: $scope.model, // your original form data,
+                data: fd, // your original form data,
                 //transformRequest: formDataObject // this sends your data to the formDataObject provider that we are defining below.
                 transformRequest: angular.identity,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
+                headers: {'Content-Type': undefined,enctype:'multipart/form-data'}
             }).
             success(function(data, status, headers, config) {
                 console.log(data);
@@ -48,7 +56,8 @@ app.controller('sign_upCtrl', function($scope, $ocLazyLoad, $http) {
             error(function(data, status, headers, config) {
                 console.log(status);
             });
-        //}
+           
+        }
 
     }
 
