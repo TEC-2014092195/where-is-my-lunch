1f669850d9a0ea9259caf8afed7abc7a4c52ce47
Kevin Hernández Rostrán <kevinah95@gmail.com>
2015-11-30 16:08:57 -0600

    Ya agragadas

--- por_agregar/allpayments $/assets/js/allpaymentsCtrl.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("allpayments",[]);
-app.controller('allpaymentsCtrl', ['$scope','$http', '$location', function($scope, $http, $location) {
-  $scope.init = function() {
-      $http.get('/api/allpayments').success(function(data) { $scope.posts = data; console.log(data); });
-      console.log("Ejecutado");
-  };
-}]);
--- por_agregar/signUp/public/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('hyperApp', ['ngRoute','routeStyles']);
-app.config(['$routeProvider', function($routeProvider) {
-
-    $routeProvider.when('/home', {
-        templateUrl: '_core/components/sign_up/sign_upView.html',
-        css: '_core/components/sign_up/sign_upStyles.css'
-    });
-
-}]);
